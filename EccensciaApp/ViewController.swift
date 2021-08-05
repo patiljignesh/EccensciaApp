98f5b2a489e2bfa24562795eba4bfd97f41e0d3f
Jignesh Patil <patiljignesh@gmail.com>
2021-08-05 13:19:57 +0200

    Initial Commit

---
+++ EccensciaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EccensciaApp
+//
+//  Created by Jigneshkumar Patil on 2021/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
